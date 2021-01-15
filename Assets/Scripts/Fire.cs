6ee9f6d5bdea4065758877f497fd6f9137d86b98
Budaházi Bence <budahazi.bence@petrik.hu>
2021-01-15 11:30:00 +0100

    Óra első felében elkészült verzió.

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public GameObject Circle;
+    public GameObject Triangle;
+    public GameObject Square;
+    public GameObject FirePoint;
+    private Vector3 position;
+    private void Start()
+    {
+        var position = FirePoint.transform.position;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Instantiate(Circle, new Vector3(position.x, position.y, 0), Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Instantiate(Triangle, new Vector3(position.x, position.y, 0), Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(Square, new Vector3(position.x, position.y, 0), Quaternion.identity);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Fixed Update called once in a fixed interval. It doesn't affected by framerate.
+    private void FixedUpdate()
+    {
+        transform.Translate(3 * Time.deltaTime, 0, 0);
+    }
+}
